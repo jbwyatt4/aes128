7558d6824b2be15a7a0e44e51ed973374c2b2b5a
John Wyatt <jbwyatt4@gmail.com>
2012-12-21 15:11:56 -0800

    added licenses to source files

--- project.c
+++ project.c
@@ -1,6 +1,24 @@
 #include "project.h"
 
 /**
+Copyright (C) 2012 John B. Wyatt IV
+License: GPLv3 by the Free Software Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
 Main function, not much to it.
 The majority of the program is accessed through checkInput
 */
--- project.h
+++ project.h
@@ -1,6 +1,23 @@
 #ifndef PROJECT_H
 #define PROJECT_H
 
+/**
+Copyright (C) 2012 John B. Wyatt IV
+License: GPLv3 by the Free Software Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 // headers
 // http://www.unix.com/programming/47881-help-compiling-header-files-c-file-using-gcc.html
 // http://mrbook.org/tutorials/make/
